dd5e1ef1b3e6bb753e65854fdf6cd9e9b18b8de0
Steve <falecomstevevaz@gmail.com>
2025-12-05 20:44:31 -0300

    Formula Computation w3resource Exercise - https://www.w3resource.com/java-exercises/basic/index.php

---
+++ src/Exercises/ExpressionEvaluation.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class ExpressionEvaluation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test formula Data: ");
+	    Double formula = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+	    System.out.printf("\n%s", formula);
+
+	}
+
+}
